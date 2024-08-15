f52219639f9e492e09dd3cf9e0fcc21c952aa99e
sabir222 <mrsabir4@gmail.com>
2024-08-15 14:18:39 +0100

    init commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const { withContentlayer } = require("next-contentlayer");
+const withNextIntl =
+  require("next-intl/plugin")();
+  // './i18n.ts'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
+
+// module.exports = nextConfig;
+module.exports = withNextIntl(withContentlayer(nextConfig));
